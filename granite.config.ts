b024871c96bf2d1503b93f0c43563237cce19961
zigsong <wldms5764@gmail.com>
2026-05-01 09:48:33 +0900

    chore: displayName 설정 변경

--- granite.config.ts
+++ granite.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from "@apps-in-toss/web-framework/config";
 export default defineConfig({
   appName: "shell-cracker",
   brand: {
-    displayName: "shell-cracker", // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
-    primaryColor: "#3182F6", // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
+    displayName: "수달의 조개깨기", // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
+    primaryColor: "#ffffff", // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
     icon: "./images/app_icon_radius_0.png", // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
   },
   web: {
