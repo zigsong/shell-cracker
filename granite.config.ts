6af3e576130879468455a35c1d5188da1112e1af
zigsong <wldms5764@gmail.com>
2026-05-06 08:05:29 +0900

    chore: 아이콘 주소 변경

--- granite.config.ts
+++ granite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   brand: {
     displayName: "수달의 조개깨기", // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
     primaryColor: "#ffffff", // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
-    icon: "./images/app_icon_radius_0.png", // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
+    icon: "https://static.toss.im/appsintoss/24565/43269d13-258f-40fc-b8b9-8b17c3b8cdae.png", // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
   },
   web: {
     host: "localhost",
